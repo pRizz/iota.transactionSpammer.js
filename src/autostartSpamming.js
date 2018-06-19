6329b90456d763a87ba01a2ba5c89d4de15e5ef7
Peter Ryszkiewicz <pryszkie@gmail.com>
2018-06-18 23:25:35 -0500

    Made package.json. Cleaning up transactionSpammer

--- src/autostartSpamming.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Peter Ryszkiewicz (https://github.com/pRizz) on 9/10/2017.
- * https://github.com/pRizz/iota.transactionSpammer.js
- */
-
-window.iotaTransactionSpammer.startSpamming()
